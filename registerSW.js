1bd64e59e52677eccacb9a367c2243162950820c
loharnimu <loharnimu@gmail.com>
2026-03-26 13:18:46 +0530

    Add files via upload

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Salary-Calculator-Pro-V2/sw.js', { scope: '/Salary-Calculator-Pro-V2/' })})}
